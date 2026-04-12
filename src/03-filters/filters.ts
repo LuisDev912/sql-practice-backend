0c12c2ff48cad6c8e24d064ab5891884d030bc97
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-12 13:14:22 -0300

    feat(03-filters): create SELECT statements with basic operators

--- src/03-filters/filters.ts
+++ src/03-filters/filters.ts
@@ -2,7 +2,14 @@ import { db } from '../db.ts';
 
 /* === SELECT + WHERE STATEMENTS === */
 
-const getUsers = db.prepare('SELECT * FROM users;').all();
+const getAllUsers = db.prepare('SELECT * FROM users;').all();
+// --- operators ---
+const johnUsers = db.prepare("SELECT * FROM users WHERE name = 'John Doe';").all();
+const belowThirtyAge = db.prepare("SELECT * FROM users WHERE age < 30;").all();
+const notDollarSalaryUsers = db.prepare("SELECT * FROM users WHERE currency != 'USD'").all();
 
 // show the information
-console.table(getUsers);
+console.table(getAllUsers);
+console.table(johnUsers);
+console.table(belowThirtyAge);
+console.table(notDollarSalaryUsers);
