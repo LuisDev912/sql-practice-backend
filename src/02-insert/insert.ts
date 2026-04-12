07df7745294af32bb39ff74776f707bf5933e9ef
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-12 13:26:28 -0300

    feat(03-filters): create SELECT queries with advanced search operators

--- src/02-insert/insert.ts
+++ src/02-insert/insert.ts
@@ -7,8 +7,8 @@ const firstInsert = db.prepare(`
 
 // --- run insert queries ---
 firstInsert.run('John Doe', 1500, 'USD', 19);
-firstInsert.run('Guillermo', 3.471465, 'ars', 37);
-firstInsert.run('DROP TABLE users); --', 3.471465, 'ars', 37);
+firstInsert.run('Guillermo', 3471465, 'ars', 37);
+firstInsert.run('DROP TABLE users); --', 3471465, 'ars', 37);
 firstInsert.run(0, '', '', '20'); // test query
 
 /* ===  SELECT STATEMENTS === */
--- src/03-filters/filters.ts
+++ src/03-filters/filters.ts
@@ -3,13 +3,22 @@ import { db } from '../db.ts';
 /* === SELECT + WHERE STATEMENTS === */
 
 const getAllUsers = db.prepare('SELECT * FROM users;').all();
+const getFirstUsers = db.prepare('SELECT * FROM users LIMIT 3;').all();
 // --- operators ---
 const johnUsers = db.prepare("SELECT * FROM users WHERE name = 'John Doe';").all();
 const belowThirtyAge = db.prepare("SELECT * FROM users WHERE age < 30;").all();
 const notDollarSalaryUsers = db.prepare("SELECT * FROM users WHERE currency != 'USD'").all();
+// --- advanced search ---
+const similarNames = db.prepare("SELECT * FROM users WHERE name LIKE '_uillermo'").all();
+const differentNames = db.prepare("SELECT * FROM users WHERE name NOT IN ('Guillermo')").all();
+const similarAges = db.prepare('SELECT * FROM users WHERE age BETWEEN 18 AND 25').all();
 
 // show the information
 console.table(getAllUsers);
+console.table(getFirstUsers);
 console.table(johnUsers);
 console.table(belowThirtyAge);
 console.table(notDollarSalaryUsers);
+console.table(similarNames);
+console.table(differentNames);
+console.table(similarAges);
