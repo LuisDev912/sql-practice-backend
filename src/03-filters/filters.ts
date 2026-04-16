078aac30458e76c2c5d55c9a4a18b59efdfa395e
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-16 17:01:26 -0300

    feat(03-filters): add queries with pagination filters

--- src/03-filters/filters.ts
+++ src/03-filters/filters.ts
@@ -2,8 +2,8 @@ import { db } from '../db.ts';
 
 /* === SELECT + WHERE STATEMENTS === */
 
+// note: it is a good practice to select the rows we want to get instead of all the rows. Here I do it this way for learning purposes.
 const getAllUsers = db.prepare('SELECT * FROM users;').all();
-const getFirstUsers = db.prepare('SELECT * FROM users LIMIT 3;').all();
 const getDifferentUsers = db.prepare('SELECT DISTINCT name FROM users;').all();
 // --- operators ---
 const johnUsers = db.prepare("SELECT * FROM users WHERE name = 'John Doe';").all();
@@ -13,10 +13,15 @@ const notDollarSalaryUsers = db.prepare("SELECT * FROM users WHERE currency != '
 const similarNames = db.prepare("SELECT * FROM users WHERE name LIKE '_uillermo'").all();
 const differentNames = db.prepare("SELECT * FROM users WHERE name NOT IN ('Guillermo')").all();
 const similarAges = db.prepare('SELECT * FROM users WHERE age BETWEEN 18 AND 25').all();
+// --- order ---
+const ascendentUsers = db.prepare('SELECT * FROM users ORDER BY name;').all(); // the ASC value is the default value
+const descendentUsers = db.prepare('SELECT * FROM users ORDER BY name DESC;').all();
+// --- pagination ---
+const getFirstUsers = db.prepare('SELECT * FROM users LIMIT 3;').all();
+const getLastUsers = db.prepare('SELECT * FROM users LIMIT 5 OFFSET 10;').all();
 
 // show the information
 console.table(getAllUsers);
-console.table(getFirstUsers);
 console.table(getDifferentUsers);
 console.table(johnUsers);
 console.table(belowThirtyAge);
@@ -24,3 +29,7 @@ console.table(notDollarSalaryUsers);
 console.table(similarNames);
 console.table(differentNames);
 console.table(similarAges);
+console.table(ascendentUsers);
+console.table(descendentUsers);
+console.table(getFirstUsers);
+console.table(getLastUsers);
