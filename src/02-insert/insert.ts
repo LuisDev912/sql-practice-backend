0acf634d50fbfd5caf2f91adadc4d3013c2a0c4e
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-17 10:03:05 -0300

    feat(02-insert): add INSERT queries for the companies table

--- src/02-insert/insert.ts
+++ src/02-insert/insert.ts
@@ -5,13 +5,28 @@ const firstInsert = db.prepare(`
     INSERT INTO users (name, salary, currency, age) VALUES (?, ?, ?, ?);
 `);
 
+const secondInsert = db.prepare(`
+    INSERT INTO companies (name) VALUES (?);
+`);
+
 // --- run insert queries ---
 firstInsert.run('John Doe', 1500, 'USD', 19);
 firstInsert.run('Guillermo', 3471465, 'ars', 37);
 firstInsert.run('DROP TABLE users); --', 3471465, 'ars', 37);
 firstInsert.run(0, '', '', '20'); // test query
 
+secondInsert.run('Bungie');
+secondInsert.run('Rockstar');
+secondInsert.run('Vercel');
+secondInsert.run('Microsoft');
+secondInsert.run('343 Industries');
+secondInsert.run('Apple');
+secondInsert.run('Globant');
+secondInsert.run('Meta');
+
 /* ===  SELECT STATEMENTS === */
-const getUsers = db.prepare('SELECT * FROM users;').all();
+const getUsers = db.prepare('SELECT name, salary, currency, age FROM users;').all();
+const getCompanies = db.prepare('SELECT name FROM companies;').all();
 
 console.table(getUsers);
+console.table(getCompanies);
