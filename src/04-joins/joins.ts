c838fc9e742a252f9dc94bd68dd77b3184715adb
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-18 10:36:06 -0300

    feat(04-joins): create a JOIN query using alias

--- src/04-joins/joins.ts
+++ src/04-joins/joins.ts
@@ -8,5 +8,13 @@ const selectJobsWithCompanies = db.prepare(`
     JOIN companies ON jobs.company_id = companies.id;
 `).all();
 
+// --- with alias ---
+const joinWithAlias = db.prepare(`
+    SELECT j.title, c.name
+    FROM jobs j
+    JOIN companies c ON j.company_id = c.id;
+`).all();
+
 /* === OUTPUT === */
 console.table(selectJobsWithCompanies);
+console.table(joinWithAlias);
