30e8ad33d5375d25f6072ae02692b49a38ce4578
Luis Calleja <luiscallejaenrique@gmail.com>
2026-04-18 10:30:39 -0300

    feat(04-joins): create a basic JOIN query

--- src/04-joins/joins.ts
+++ src/04-joins/joins.ts
@@ -1,2 +1,12 @@
 import { db } from '../db.ts';
 
+/* === JOINS === */
+
+// --- basic join ---
+const selectJobsWithCompanies = db.prepare(`
+    SELECT jobs.title, companies.name FROM jobs
+    JOIN companies ON jobs.company_id = companies.id;
+`).all();
+
+/* === OUTPUT === */
+console.table(selectJobsWithCompanies);
