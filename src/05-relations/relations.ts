1e4b5143d61fc3b8352e748c0041ea4e44d17675
Luis Calleja <luiscallejaenrique@gmail.com>
2026-05-11 15:49:58 -0300

    feat(05-relations): add a basic example of GROUP_CONCAT

--- src/05-relations/relations.ts
+++ src/05-relations/relations.ts
@@ -1,2 +1,9 @@
 import { db } from '../db.ts';
 
+const jobsWithTech = db.prepare(`
+    SELECT j.id, j.title, GROUP_CONCAT(technology)
+    FROM jobs j
+    JOIN job_technologies ON id = job_id;
+`).all();
+
+console.table(jobsWithTech);
