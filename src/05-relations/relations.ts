72fd5ee535096f61db57e8f3cd0156f013ec2738
Luis Calleja <luiscallejaenrique@gmail.com>
2026-06-14 09:40:56 -0300

    refactor(05-relations): improve query to show jobs with multiple technologies

--- src/05-relations/relations.ts
+++ src/05-relations/relations.ts
@@ -1,9 +1,12 @@
 import { db } from '../db.ts';
 
 const jobsWithTech = db.prepare(`
-    SELECT j.id, j.title, GROUP_CONCAT(technology)
+    SELECT 
+        j.title,
+        GROUP_CONCAT(jt.technology) AS technologies
     FROM jobs j
-    JOIN job_technologies ON id = job_id;
+    JOIN job_technologies jt ON j.id = jt.job_id
+    GROUP BY j.id;
 `).all();
 
 console.table(jobsWithTech);
