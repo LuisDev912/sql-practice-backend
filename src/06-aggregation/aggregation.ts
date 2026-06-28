af524091fbf9e6e7294f0ec2a11c6ce6171b33ea
Luis Calleja <luiscallejaenrique@gmail.com>
2026-06-28 10:30:41 -0300

    feat(06-aggregations): add an example query for conditionals

--- src/06-aggregation/aggregation.ts
+++ src/06-aggregation/aggregation.ts
@@ -1,1 +1,13 @@
 import { db } from '../db.ts';
+
+const usersByAge = db.prepare(`
+    SELECT name, age,
+        CASE
+            WHEN age < 26 THEN 'young'
+            WHEN age <= 45 then 'young adult'
+            ELSE 'adult'
+        END as user_age
+    FROM users;
+`).all();
+
+console.table(usersByAge);
