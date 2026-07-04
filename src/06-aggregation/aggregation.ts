dafcdd52802321f43bb3f3cc2f875fd7b76ad7c6
Luis Calleja <luiscallejaenrique@gmail.com>
2026-07-04 12:45:30 -0300

    refactor(06-aggregation): improve column names of aggregation queries

--- src/06-aggregation/aggregation.ts
+++ src/06-aggregation/aggregation.ts
@@ -45,27 +45,27 @@ const roundedUsersSalary = db.prepare(`
 
 /* === AGGREGATION === */
 const countRows = db.prepare(`
-    SELECT COUNT(*) AS rows FROM jobs;
+    SELECT COUNT(*) AS total_rows FROM jobs;
 `).all();
 
 const sumSalaries = db.prepare(`
-    SELECT name, SUM(salary) AS salary FROM users;
+    SELECT name, SUM(salary) AS salary_sum FROM users;
 `).all();
 
 const averageUserAge = db.prepare(`
-    SELECT name, AVG(age) AS age FROM users;
+    SELECT name, AVG(age) AS average_age FROM users;
 `).all();
 
 const roundAverageUserAge = db.prepare(`
-    SELECT ROUND(AVG(age)) AS age FROM users;
+    SELECT ROUND(AVG(age)) AS average_age FROM users;
 `).all();
 
 const minUserAge = db.prepare(`
-    SELECT name, MIN(age) AS age FROM users;
+    SELECT name, MIN(age) AS min_age FROM users;
 `).all();
 
 const maxUserAge = db.prepare(`
-    SELECT name, MAX(age) AS age FROM users;
+    SELECT name, MAX(age) AS max_age FROM users;
 `).all();
 
 /* === LOGS ===*/
