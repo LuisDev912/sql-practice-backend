7e37860f4dea1c4c7b58f7aa133e7b3ae0e846ed
Luis <luiscallejaenrique@gmail.com>
2026-07-16 12:20:28 -0300

    feat(07-subqueries-and-ctes): create the first subquery

--- src/07-subqueries-and-ctes/subqueriesAndCte.ts
+++ src/07-subqueries-and-ctes/subqueriesAndCte.ts
@@ -2,8 +2,14 @@ import { db } from '../db.ts';
 
 /* === SUBQUERIES === */
 
+const usersWithOldestAge = db.prepare(`
+    SELECT name, age 
+    FROM users
+    WHERE age = (SELECT MAX(age) FROM users);    
+`).all();
 
 /* === CTES === */
 
 
 /* === LOGS ===*/
+console.table(usersWithOldestAge);
